431370453b706585337fe392b025464d0b587973
X Tian <31002331+xintian7@users.noreply.github.com>
2022-06-01 11:26:49 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  rootNotionPageId: '4817bc3b1ccc440db72183e6a3e0c139',
+  
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Werkworden',
+  domain: 'Werkworden.nl',
+  author: 'Xin Tian',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
